f9fc541f6431725a5952ccdc607602f38d030b73
wikiopt <74967986+wikiopt@users.noreply.github.com>
2020-12-23 17:34:29 +0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,7 +13,3 @@ exports.onRouteUpdate = function (_ref) {
     domElem.setAttribute("content", "" + value);
   }
 };
-import "bootstrap/dist/css/bootstrap.min.css";
-import "jquery/dist/jquery.min.js";
-import "popper.js/dist/popper.min";
-import "bootstrap/dist/js/bootstrap.min.js";
